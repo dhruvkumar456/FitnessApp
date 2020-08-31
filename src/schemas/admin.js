05f7c6c27ddf707b4b0b42342a565267160dac82
kritik dalakoti <dalakotikritik@gmail.com>
2020-08-31 12:10:56 +0530

    admin schema include

---
+++ src/schemas/admin.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose')
+
+const adminschema=mongoose.Schema({
+    videono:{
+        type:String,
+        required:true
+    },
+    calorie:{
+        type:Number,
+        required:true
+    },
+    likes:{
+        type:Number,
+        required:true
+    },
+    dislikes:{
+        type:Number,
+        required:true
+    }
+})
+
+const Admin=mongoose.model('Admin',libraryschema)
+module.exports=Admin
